3f668472609afd7a1d22a75cd870e8f3755bfcd4
Espryra <espryra@gmail.com>
2025-12-28 06:48:59 -0700

    Added debug logging.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import * as fs from "fs";
+import Handler from "./modules/handler";
+import Logger from "./utils/logger";
 
 if (!fs.existsSync("data")) {
+  Logger.Debug("Creating data directory...");
   fs.mkdirSync("data/backups", { recursive: true });
 }
